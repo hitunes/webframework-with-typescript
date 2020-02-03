17b1887a88191b5172c51e7dbd1b177d89178ecb
hitunes <hitunes247@gmail.com>
2020-02-03 10:39:02 +0200

    Trigegring event callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { User } from "./models/User";
 
 const user = new User({name: 'itunu', age: 20});
+
+user.set({name: 'notmyname'})
+user.on('click', () => {});
+user.on('click', () => {});
+user.on('click', () => {});
+
+console.log(user)
