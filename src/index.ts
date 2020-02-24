28bcff4aa737b2389b9dc755186a8d5aa2090347
hitunes <hitunes247@gmail.com>
2020-02-24 10:54:55 +0200

    Round up Webframe mirroring MArrionetteJs and backboneJs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
+import { User, UserProps } from './models/User';
 
-const user = User.buildBuild({name: 'NAME', age: 21})
+const users = new Collection('http://localhost:4000/users', (json: UserProps) => {
+  return User.buildBuild(json)
+})
+users.on('change', () => {
+  const root = document.getElementById('root')
 
-const root = document.getElementById('root');
-
-if (root){
-  const userForm = new UserForm(root, user)
-
-  userForm.render()
-}
+  if(root){
+    new UserList(root, users).render();
+  }
+})
+users.fetch();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,13 @@
-import { User } from '../models/User';
-export class UserForm {
-  constructor(public parent: Element, public model: User){
-    this.bindModel();
-  }
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    })
-  }
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+
+export class UserForm extends View<User, UserProps>{
   onSetAgeClick = (): void =>{
     this.model.setRandomAge();
   }
-
+  onSaveClick = (): void =>{
+    this.model.save();
+  }
   onSetNameClick = (): void =>{
     const input = this.parent.querySelector('input');
     if(input){
@@ -25,37 +21,17 @@ export class UserForm {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.change-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     }
   }
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}"/>
         <button class= "change-name">Change Name</button>
         <button class="set-age"> Set random age </button>
+        <button class="save-model"> Save User </button>
       </div>
     `
-  }
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsmap = this.eventsMap()
-    for(let eventKey in eventsmap){
-      const [eventName, selector] = eventKey.split(':');
-      fragment.querySelectorAll(selector).forEach(element=>{
-        element.addEventListener(eventName, eventsmap[eventKey])
-      })
-    }
-  }
-  render(): void {
-    this.parent.innerHTML = '';
-
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content)
-
-    this.parent.append(templateElement.content);
   }
 }
